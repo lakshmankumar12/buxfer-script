a1252968c914c075a70fa4eceb917b9c15acda3c
lakshman kumar <lakshmankumar@gmail.com>
2017-03-12 13:59:38 -0700

    take file name as arg, write to o/p file

--- buxfer.py
+++ buxfer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import csv
 import sys
 
@@ -13,7 +14,14 @@ def add_to_tag(tag_split,tag,amount,detail):
   detail.append(f)
   tag_split[tag][1].append(detail)
 
-with open("transactions.csv","rb") as csfile:
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("fileToOpen",      help="Typically the transactions.csv (this is a mandatory argment)")
+parser.add_argument("fileToWriteTo",   help="output file (this is a mandatory argment)", nargs="?", default="splitOp")
+
+parsed_args = parser.parse_args()
+
+with open(parsed_args.fileToOpen,"rb") as csfile:
   values = csv.reader(csfile)
   # skip first line always
   next(values)
@@ -22,7 +30,7 @@ with open("transactions.csv","rb") as csfile:
        #Date,Description,Currency,Amount,Type,Tags,Account,Status,Memo,Loans
        date,description,currency,amount,type,tag,account = transaction[:7]
     except ValueError as e:
-        print "Got valueError in line:%s, err:%s"%(transaction, str(e))
+        print("Got valueError in line:%s, err:%s"%(transaction, str(e)))
         sys.exit(1)
     if type != "Expense":
       continue
@@ -34,20 +42,21 @@ with open("transactions.csv","rb") as csfile:
           tag_name,value = each_tag.split(":")
           add_to_tag(tag_split,tag_name,value,[date,description])
         except ValueError:
-          print "%s has trouble in name,value -- %s"%(each_tag,transaction)
+          print("%s has trouble in name,value -- %s"%(each_tag,transaction))
     else:
       add_to_tag(tag_split,tag,amount,[date,description])
 
 sorted_tags = tag_split.keys()
 sorted_tags.sort(key=lambda tag: tag_split[tag][0])
 
-for i in sorted_tags:
-  print "Tag: %s"%i
-  items = tag_split[i][1]
-  items.sort(key=lambda i: i[2])
-  for j in tag_split[i][1]:
-    print "  %-20s %15.2lf %s"%(j[0],j[2],j[1])
+with open(parsed_args.fileToWriteTo, "w") as fd:
+    for i in sorted_tags:
+      print("Tag: %s"%i, file=fd)
+      items = tag_split[i][1]
+      items.sort(key=lambda i: i[2])
+      for j in tag_split[i][1]:
+        print("  %-20s | %15.2lf  | %s"%(j[0],j[2],j[1]), file=fd)
 
-for i in sorted_tags:
-  print "%-30s  %15.2lf"%(i,tag_split[i][0])
+    for i in sorted_tags:
+      print("%-30s | %15.2lf"%(i,tag_split[i][0]), file=fd)
 
